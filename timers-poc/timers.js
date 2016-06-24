b02600cd9b88e1054926b9d1b1a24c310b84cff5
Xcelled <j12@ketelaar.com>
2016-06-24 17:09:53 -0400

    Added Timer and Display to POC

--- timers-poc/timers.js
+++ timers-poc/timers.js
@@ -1,4 +1,23 @@
+/*
+Timing functionality is provided by three classes of objects:
+- Generators
+	Generators are responsible for taking a list of events and other args
+	and creating an iterator-style interface for them. They have three main
+	methods: next(), time(), value(). Time returns a moment() object that
+	represents the absolute time the event should be triggered.
 
+- Timer
+	The timer takes the output of a generator and uses it to schedule events.
+	It maintains a list of future events, which can be queried. They can notify
+	other objects when they tick.
+
+- Displays
+	Displays take Timers and retrieve information from them, which they then
+	format for... uh... display. Displays hook the timer's tick event.
+
+Most of the ugly work is done in generators, in an effort to have a clear and
+extensible APi at the level of timers and displays.
+*/
 
 // Time-tick generator types:
 // 	Rotate: Interval Based
@@ -24,7 +43,7 @@ generators.select_erinn = function(args, list) {
 	// Parses a datetime into an hour/minute
 	function parse(time) {
 		var hhmm = time.split(":");
-		return {'hour' : parseInt(hhmm[0]), 'minute': parseInt(hhmm[0])};
+		return {'hour' : parseInt(hhmm[0]), 'minute': parseInt(hhmm[1])};
 	}
 
 	self.entries = [];
@@ -77,13 +96,11 @@ generators.select_erinn = function(args, list) {
 // Wraps a generator in a compressing function
 // Compressing just eats consecutive generator results
 // that have the same text
-function compressor(generator) {
-	var self = {};
-
-	// Retrives the next compressed entry
-	self.next = function() {
-		var currentValue = self.value();
-		while (self.value() == currentValue) {
+function Compressor(generator) {
+	// Retrieves the next compressed entry
+	this.next = function() {
+		var currentValue = this.value();
+		while (this.value() == currentValue) {
 			if (!generator.next())
 				return false;
 		}
@@ -91,8 +108,111 @@ function compressor(generator) {
 		return true;
 	}
 
-	self.time = function() { return generator.time(); }
-	self.value = function() { return generator.value(); }
+	this.time = function() { return generator.time(); }
+	this.value = function() { return generator.value(); }
+}
 
-	return self;
+// --------------- Display classes --------------------------
+jQuery.fn.style = function(x) {
+	if(typeof(x) !== "undefined") this.attr("style", x);
+	return this;
+}
+
+function Display($elem, args, timer) {
+	var skip = args.skip ? args.skip[0] : 0;
+	var show = args.show ? args.show[0] : 2;
+
+	// Create display widget...
+	var $container = $("<div>").addClass("display-time-").style(args.displayStyle).appendTo($elem);
+	if (args.label) {
+		var $head = $("<span>").addClass("timer-label").style(args.labelStyle).html(args.label[0])
+		$head.appendTo($container);
+	}
+	var $display = $("<div>").style(args.listStyle).appendTo($container);
+
+	this.update = function() {
+		// Clear it out
+		$display.empty();
+
+		// Add new items
+		for (var i = skip; i < show+skip; i++) {
+			var label = timer.value(i), time = timer.time(i);
+
+			$display.append($("<div>").addClass("display-entry").style(args.entryStyle)
+				.append($("<span>").addClass("display-time").style(args.timeStyle).text(time.local().format("h:mm:ss a")))
+				.append($("<span>").addClass("display-label").style(args.valueStyle).html(label))
+			);
+		}
+	}
+
+	this.increase = function () {
+		++show;
+		self.update();
+	}
+
+	this.decrease = function () {
+		if (show > 1) {
+			--show;
+			self.update();
+		}
+	}
+
+	// Register tick handler
+	timer.onTick(this.update);
+
+	// Initialize
+	this.update();
+}
+
+// ----------------------------- Timer class -----------------------------------
+// Contains logic for triggering on changes
+function Timer(generator) {
+	var ticks = [];
+	var listeners = [];
+	var self = this;
+
+	this.onTick = function(handler) {
+		if (typeof(handler) !== "function") return;
+
+		listeners.push(handler);
+	}
+
+	this.get = function(idx) {
+		while (ticks.length <= idx) {
+			if (!generator.next()) {
+				console.warn('No more time entries to retrieve');
+				return [undefined, undefined];
+			}
+			ticks.push([generator.time(), generator.value()]);
+		}
+
+		return ticks[idx];
+	}
+
+	this.time = function(idx) { return this.get(idx)[0]; }
+	this.value = function(idx) { return this.get(idx)[1]; }
+
+	function registerNextChange() {
+		var next = self.time(1); // Next time change
+		if (next) {
+			setTimeout(self._tocker, next.diff(moment()));
+		}		
+	}
+
+	this._tocker = function() {
+		// TODO: Maybe check that the time actually *has* passed?
+		// Pop 'current (expired)' off, makes index 1 new current.
+		ticks.shift();
+		// Trigger update
+		for (var i = 0; i < listeners.length; i++) {
+			listeners[i](this);
+		}
+
+		registerNextChange();
+	}
+
+	// Initialize
+	ticks.push([generator.time(), generator.value()]);
+
+	registerNextChange();
 }
