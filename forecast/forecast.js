87e9a93c9a246f9fe98a8d554de2a8c9ee63cb87
Scott Ketelaar <git@scottketelaar.com>
2017-03-01 13:11:10 -0500

    Refactored time

--- forecast/forecast.js
+++ forecast/forecast.js
@@ -16,8 +16,8 @@
 //     Abb Neagh Residential, Sliab Cuilin Residential + Castle
 // Unchecked: Doki Doki, Beginner zones, AW, Falias
 
-angular.module('forecast', ['time'])
-	.factory('forecastSvc', ['$http', 'timeSvc', function ($http, timeSvc) {
+angular.module('forecast', ['time', 'angularMoment'])
+	.factory('forecastSvc', ['$http', 'moment', function ($http, moment) {
 		// TODO: Make i18n a service?
 		var l18n = {
 			"type1": "Northern Uladh",
@@ -111,7 +111,7 @@ angular.module('forecast', ['time'])
 							next: []
 						}
 
-						var time = timeSvc.boundDown(moment(from).add(20, 'minutes'));
+						var time = boundDown(moment(from).add(20, 'minutes'));
 						for (var i = 1; i < forecast.length; ++i) {
 							area.next.push({ time: time, intensity: forecast[i], desc: intensity(forecast[i], id) });
 							time.add(20, 'minutes');
--- time/time.js
+++ time/time.js
@@ -1,112 +1,112 @@
+// Constants
+var SERVER_TIMEZONE = "America/Los_Angeles";
+
+var MS_PER_ERINN_MINUTE = 1500;
+var MS_PER_ERINN_HOUR = MS_PER_ERINN_MINUTE * 60;
+var MS_PER_ERINN_DAY = MS_PER_ERINN_HOUR * 24;
+
+var TIME_PER_ERINN_MINUTE = MS_PER_ERINN_MINUTE; // Backwards compat
+var TIME_PER_ERINN_HOUR = MS_PER_ERINN_HOUR; // Backwards compat
+var TIME_PER_ERINN_DAY = MS_PER_ERINN_DAY; // Backwards compat
+
+var MS_SCALE_FACTOR = 40;
+
+var ERINN_EPOCH = moment.utc('0001-01-01');
+
+// Patch moment with some useful methods
+
+moment.fn.isNight = function () {
+	return this.hour() < 6 || this.hour() >= 18
+};
+
+/**
+ * @function toErinn
+ * @description Converts a time from realtime to Erinn time
+ * 
+ * @returns moment
+ */
+moment.fn.toErinn = function () {
+	// convert realTime to UTC equiv to avoid DST issues
+	var realTime = this.clone().utc().add(this.utcOffset(), 'minutes');
+	var elapsed = (realTime - ERINN_EPOCH);
+	return ERINN_EPOCH.clone().millisecond(elapsed * MS_SCALE_FACTOR);
+};
+
+/**
+ * @function toReal
+ * @description Converts Erinn time to real time
+ * 
+ * @returns moment
+ */
+moment.fn.toReal = function () {
+	var elapsed = (this - ERINN_EPOCH);
+	var rt = ERINN_EPOCH.clone().add(elapsed / MS_SCALE_FACTOR, 'milliseconds').tz(SERVER_TIMEZONE);
+	return rt.subtract(rt.utcOffset(), 'minutes');
+};
+
+/**
+ * @function realToErinn
+ * @description Converts a realtime duration to its Erinn equiv
+ * 
+ * @returns moment.duration
+ */
+moment.duration.fn.toErinn = function () {
+	return moment.duration(this.asMilliseconds() * MS_SCALE_FACTOR);
+};
+
+/**
+ * @function erinnToReal
+ * @description Converts an erinn duration to is realtime equiv
+ * 
+ * @returns moment.duration
+ */
+moment.duration.fn.toReal = function () {
+	return moment.duration(duration.asMilliseconds() / MS_SCALE_FACTOR);
+}
+
 /**
  * @module time
  */
-angular.module('time', [])
-	.factory('timeSvc', ['$timeout', function ($timeout) {
-		// Constants
-		var SERVER_TIMEZONE = "America/Los_Angeles";
-
-		var MS_PER_ERINN_MINUTE = 1500;
-		var MS_PER_ERINN_HOUR = MS_PER_ERINN_MINUTE * 60;
-		var MS_PER_ERINN_DAY = MS_PER_ERINN_HOUR * 24;
-
-		var TIME_PER_ERINN_MINUTE = MS_PER_ERINN_MINUTE; // Backwards compat
-		var TIME_PER_ERINN_HOUR = MS_PER_ERINN_HOUR; // Backwards compat
-		var TIME_PER_ERINN_DAY = MS_PER_ERINN_DAY; // Backwards compat
-
-		var MS_SCALE_FACTOR = 40;
-
-		var ERINN_EPOCH = moment.utc('0001-01-01');
+angular.module('time', ['angularMoment'])
+	.factory('hhmmService', function () {
+		var service = {
+			parse: parse,
+			diff: diff
+		};
 
 		/**
-		 * @function erinnTime
-		 * @description makes a new Erinn Time
+		 * @function parse
+		 * @description Parses a string formatted as "HH:MM" to an object
+		 * 
+		 * @returns {hour, minute}
 		 */
-		function erinnTime(day, hour, minute) {
-			return {
-				day: day,
-				hour: hour,
-				minute: minute,
-				isNight: hour < 6 || hour >= 18
-			};
+		function parse(time) {
+			var hhmm = time.split(":");
+			return { 'hour': parseInt(hhmm[0]), 'minute': parseInt(hhmm[1]) };
 		}
 
-		var convert = {
-			/**
-			 * @function realToErinn
-			 * @description Converts a time from realtime to Erinn time
-			 * 
-			 * @returns {day, hour, minute}
-			 */
-			realToErinn: function (realTime) {
-				// convert realTime to UTC equiv to avoid DST issues
-				realTime = realTime.clone().utc().add(realTime.utcOffset(), 'minutes');
-				var elapsed = (realTime - ERINN_EPOCH);
+		/**
+		 * @function diff
+		 * @description Calculates the difference between two HHMM times
+		 * 
+		 * @return number of minutes
+		 */
+		function diff(a, b) {
+			return (a.hour * 60 + a.minute) - (b.hour * 60 + b.minute);
+		}
 
-				return erinnTime(
-					Math.floor(elapsed / MS_PER_ERINN_DAY),
-					Math.floor(elapsed / MS_PER_ERINN_HOUR) % 24,
-					Math.floor(elapsed / MS_PER_ERINN_MINUTE) % 60
-				);
-			},
-
-			/**
-			 * @function erinnToReal
-			 * @description Converts Erinn time to real time
-			 * 
-			 * @returns moment
-			 */
-			erinnToReal: function (erinn) {
-				var elapsed = erinn.day * MS_PER_ERINN_DAY + erinn.hour * MS_PER_ERINN_HOUR + erinn.minute * MS_PER_ERINN_MINUTE;
-
-				var rt = ERINN_EPOCH.clone().add(elapsed, 'milliseconds').tz(SERVER_TIMEZONE);
-				return rt.subtract(rt.utcOffset(), 'minutes');
-			}
+		return service;
+	})
+	.factory('clockService', ['$timeout', 'moment', function ($timeout, moment) {
+		var now = {
+			real: getServerTime(),
+			erinn: getErinnTime()
 		};
 
-		var duration = {
-			/**
-			 * @function realToErinn
-			 * @description Converts a realtime duration to its Erinn equiv
-			 * 
-			 * @returns moment.duration
-			 */
-			realToErinn: function (duration) {
-				return moment.duration(duration.asMilliseconds() * MS_SCALE_FACTOR);
-			},
-
-			/**
-			 * @function erinnToReal
-			 * @description Converts an erinn duration to is realtime equiv
-			 * 
-			 * @returns moment.duration
-			 */
-			erinnToReal: function (duration) {
-				return moment.duration(duration.asMilliseconds() / MS_SCALE_FACTOR);
-			}
-		};
-
-		hhmm = {
-			/**
-			 * @function parse
-			 * @description Parses a string formatted as "HH:MM" to an object
-			 * 
-			 * @returns {hour, minute}
-			 */
-			parse: function (time) {
-				var hhmm = time.split(":");
-				return { 'hour': parseInt(hhmm[0]), 'minute': parseInt(hhmm[1]) };
-			},
-
-			/**
-			 * @function diffHHMM
-			 * @description Calculates the difference between two HHMM times
-			 * 
-			 * @return number of minutes
-			 */
-			diff: function (a, b) {
-				return (a.hour * 60 + a.minute) - (b.hour * 60 + b.minute);
-			}
+		var service = {
+			getServerTime: getServerTime,
+			getErinnTime: getErinnTime,
+			now: function () { return now; }
 		};
 
 		// Convenience Functions
@@ -122,23 +122,18 @@ angular.module('time', [])
 		/**
 		 * @function getErinnTime
 		 * 
-		 * @returns {}
+		 * @returns moment
 		 */
 		function getErinnTime() {
-			return convert.realToErinn(getServerTime());
+			return getServerTime().toErinn();
 		}
-
-		var now = {
-			real: getServerTime(),
-			erinn: getErinnTime()
-		};
 
 		function updateErinnTime() {
 			now.erinn = getErinnTime();
-			var time = getErinnTime(); // Get it again since we wasted time in handlers
+			var next = getErinnTime().add(1, 'minute').second(0).millisecond(0); // Get it again since we wasted time in handlers
 
 			// set up next round
-			$timeout(updateErinnTime, convert.erinnToReal(erinnTime(time.day, time.hour, time.minute + 1)).diff(getServerTime()));
+			$timeout(updateErinnTime, next.toReal().diff(getServerTime()));
 		}
 		updateErinnTime();
 
@@ -150,34 +145,43 @@ angular.module('time', [])
 		updateRealTime();
 
 		// Make the service
+		return service;
+	}])
+	.filter('isNight', function() {
+		return function (erinnTime) {
+			return erinnTime.isNight();
+		};
+	})
+	.filter('isDay', function() {
+		return function (erinnTime) {
+			return !erinnTime.isNight();
+		};
+	})
+	.directive('serverClock', function () {
 		return {
-			/**
-			 * @property convert
-			 * @description Functions for converting datetimes
-			 */
-			convert: convert,
-
-			/**
-			 * @property duration
-			 * @description Functions for dealing with durations
-			 */
-			duration: duration,
-
-			/**
-			 * @property hhmm
-			 * @description Helpers for dealing with hhmm times
-			 */
-			hhmm: hhmm,
-
-			getServerTime: getServerTime,
-			getErinnTime: getErinnTime,
-			now: function () { return now; }
-		}
-	}])
-	.directive('timeServerCurrent', ['timeSvc', function(timeSvc) {
-
-	}])
-	.controller('timeCtrl', ['timeSvc', function(timeSvc) {
+			restrict: 'AEC',
+			template: "{{ clock.now().real | amDateFormat:'h:mm a' }}"
+		};
+	})
+	.directive('erinnClock', function () {
+		return {
+			restrict: 'AEC',			
+			template: "{{ clock.now().erinn | amDateFormat:'HH:mm' }}"
+		};
+	})
+	.directive('isNight', function () {
+		return {
+			restrict: 'AEC',
+			template: "{{ clock.now().erinn | isNight }}"
+		};
+	})
+	.directive('isDay', function () {
+		return {
+			restrict: 'AEC',
+			template: "{{ clock.now().erinn | isDay }}"
+		};
+	})
+	.controller('clockCtrl', ['clockService', function (clockService) {
 		var self = this;
-		self.now = timeSvc.now;
+		self.now = clockService.now;
 	}]);
