1820b876fda4412e5388f117e788e5b404b80cef
Scott Ketelaar <git@scottketelaar.com>
2017-03-03 12:38:59 -0500

    Added basic localization

--- mss/mss.js
+++ mss/mss.js
@@ -1,4 +1,79 @@
-mss = angular.module('serverStat', ['angular-svg-round-progressbar']);
+mss = angular.module('serverStat', ['angular-svg-round-progressbar', 'pascalprecht.translate']);
+
+mss.config(function ($translateProvider) {
+	var translations = {
+		state: {
+			offline: 'Offline',
+			maintenance: 'Maintenance',
+			online: 'Online',
+			busy: 'Busy',
+			full: 'Full',
+			bursting: 'Bursting',
+			booting: 'Starting',
+			error: 'Error'
+		},
+		game: {
+			state: {
+				offline: 'Mabinogi is currently offline',
+				maintenance: 'Mabinogi is undergoing maintenance',
+				online: 'Mabinogi is online',
+				busy: 'Mabinogi is busy',
+				full: 'Mabinogi is almost full',
+				bursting: 'Mabinogi is at maximum capacity',
+				booting: 'Mabinogi servers are booting up...',
+				error: 'There is an error with the servers'
+			},
+			stress: {
+				stress: '{{ stress }}% full',
+				na: 'Load not available'
+			}
+		},
+		server: {
+			state: {
+				offline: 'This server is offline',
+				maintenance: 'This server is currently undergoing maintenance',
+				online: 'Online',
+				busy: 'Busy',
+				full: 'Full',
+				bursting: 'Bursting',
+				booting: 'This server is currently starting, please wait a little',
+				error: 'This server currently has an error'
+			},
+			names: {
+				mabius1: 'Mari',
+				mabius2: 'Ruairi',
+				mabius3: 'Tarlach',
+				mabius4: 'Alexina'
+			}
+		},
+		channel: {
+			state: {
+				offline: 'This channel is offline',
+				maintenance: 'This channel is currently undergoing maintenance',
+				online: 'Online',
+				busy: 'Busy',
+				full: 'Full',
+				bursting: 'Bursting',
+				booting: 'This channel is currently starting, please wait a little',
+				error: 'This channel currently has an error'
+			},
+			name: {
+				Ch1: 'Channel 1',
+				Ch2: 'Channel 2',
+				Ch3: 'Channel 3',
+				Ch4: 'Channel 4',
+				Ch5: 'Channel 5',
+				Ch6: 'Channel 6',
+				Ch7: 'Channel 7',
+				HCh: 'Housing Channel',
+			}
+		}
+	};
+
+	$translateProvider
+		.translations('en', translations)
+		.preferredLanguage('en');
+});
 
 mss.factory('statusService', ['$http', function ($http) {
 	var service = {
@@ -75,9 +150,18 @@ mss.controller('serverStatCtrl', ['statusService', function (statusService) {
 	statusService.get().then(function (status) {
 		vm.status = status;
 	});
+
+	vm.channelComparator = function (a, b) {
+		if (a.value === 'HCh')
+			return 1;
+		else if (b.value === 'HCh')
+			return -1;
+		else
+			return (a.value < b.value) ? -1 : 1;
+	}
 }]);
 
-mss.filter('stateColor', function() {
+mss.filter('stateColor', function () {
 	var colors = {
 		online: 'green',
 		busy: '#FFBB00',
@@ -95,7 +179,7 @@ mss.filter('stateColor', function() {
 	};
 });
 
-mss.filter('ratePing', function() {
+mss.filter('ratePing', function () {
 	return function (ping) {
 		if (ping <= 0) return 'off';
 		if (ping <= 50) return 'low';
