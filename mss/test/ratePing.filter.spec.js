4f3522aa49804b5f80717c100640f8c81262ec08
Scott Ketelaar <git@scottketelaar.com>
2017-03-04 22:33:39 -0500

    Fleshed out tests for ratePing

--- mss/test/ratePing.filter.spec.js
+++ mss/test/ratePing.filter.spec.js
@@ -8,4 +8,36 @@ describe('ratePing', function () {
 	it('returns off for 0', function () {
 		expect(ratePing(0)).toBe('off');
 	});
+
+	it('returns low for 1', function () {
+		expect(ratePing(1)).toBe('low');
+	});
+
+	it('returns low for 49', function () {
+		expect(ratePing(49)).toBe('low');
+	});
+
+	it('returns low for 50', function () {
+		expect(ratePing(50)).toBe('low');
+	});
+
+	it('returns medium for 51', function () {
+		expect(ratePing(51)).toBe('medium');
+	});
+
+	it('returns medium for 499', function () {
+		expect(ratePing(499)).toBe('medium');
+	});
+
+	it('returns medium for 500', function () {
+		expect(ratePing(500)).toBe('medium');
+	});
+
+	it('returns high for 501', function () {
+		expect(ratePing(501)).toBe('high');
+	});
+
+	it('returns high for 2000', function () {
+		expect(ratePing(2000)).toBe('high');
+	});
 });
