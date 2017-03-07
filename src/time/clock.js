228aaf62f992993bca8d9825714ca9641346a869
Scott Ketelaar <git@scottketelaar.com>
2017-03-07 15:43:06 -0500

    Add time lib

---
+++ src/time/clock.js
@@ -0,0 +1,78 @@
+import './patch';
+import moment from 'moment';
+import 'moment-timezone';
+import { SERVER_TIMEZONE } from './constants'
+
+export default {
+    getServerTime,
+    getErinnTime,
+    onRealTimeTicked,
+    offRealTimeTicked,
+    onErinnTimeTicked,
+    offErinnTimeTicked
+};
+
+let handlers = {
+    real: new Set(),
+    erinn: new Set()
+};
+
+// Start the tickers
+updateErinnTime();
+updateRealTime();
+
+/**
+ * @function getServerTime
+ * 
+ * @returns moment
+ */
+function getServerTime() {
+    return moment().tz(SERVER_TIMEZONE)
+}
+
+/**
+ * @function getErinnTime
+ * 
+ * @returns moment
+ */
+function getErinnTime() {
+    return getServerTime().toErinn();
+}
+
+function updateErinnTime() {
+    trigger(handlers.erinn, getErinnTime());
+    var next = getErinnTime().add(1, 'minute').second(0).millisecond(0); // Get it again since we wasted time in handlers
+
+    // set up next round
+    setTimeout(updateErinnTime, next.toReal().diff(getServerTime()));
+}
+
+function updateRealTime() {
+    trigger(handlers.real, getServerTime());
+
+    setTimeout(updateRealTime, 1000 - moment().milliseconds());
+}
+
+/**
+ * @description invokes handlers for an event
+ * @param {*} handlers 
+ * @param {*} time 
+ */
+function trigger(handlers, time) {
+    for (let handler of handlers) {
+        handler(time);
+    }
+}
+
+function on(handlers, newHandler) {
+    handlers.add(newHandler);
+}
+
+function off(handlers, oldHandler) {
+    handers.delete(oldHandler);
+}
+
+function onRealTimeTicked(handler) { on(handlers.real, handler); }
+function offRealTimeTicked(handler) { off(handlers.real, handler); }
+function onErinnTimeTicked(handler) { on(handlers.erinn, handler); }
+function offErinnTimeTicked(handler) { off(handlers.erinn, handler); }
---
+++ src/time/constants.js
@@ -0,0 +1,11 @@
+import moment from 'moment';
+
+export const SERVER_TIMEZONE = "America/Los_Angeles";
+
+export const MS_PER_ERINN_MINUTE = 1500;
+export const MS_PER_ERINN_HOUR = MS_PER_ERINN_MINUTE * 60;
+export const MS_PER_ERINN_DAY = MS_PER_ERINN_HOUR * 24;
+ 
+export const MS_SCALE_FACTOR = 40;
+ 
+export const ERINN_EPOCH = moment.utc('0001-01-01');
